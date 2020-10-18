f6a05afe394f74788712bf89faeca62bef957bda
dextermao92 <wmao003@gmail.com>
2020-10-18 12:59:16 +1100

    Updates

---
+++ precache-manifest.ba51bd9b1ceefee9a62625fbb610f2a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e3374e6f0f8f5c4ebf671e2a202738",
+    "url": "/Datathon-2020/index.html"
+  },
+  {
+    "revision": "14756a269c1cbe020829",
+    "url": "/Datathon-2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "/Datathon-2020/static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Datathon-2020/static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14756a269c1cbe020829",
+    "url": "/Datathon-2020/static/js/main.8fee0f92.chunk.js"
+  },
+  {
+    "revision": "62ca33c01db0d347d11a",
+    "url": "/Datathon-2020/static/js/runtime-main.b637958d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Datathon-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
